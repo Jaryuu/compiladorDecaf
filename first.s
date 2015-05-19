8c47174056d1ae70c5b7a4dc936f67be5dcdd654
Julio Ayala <ayala.j@live.com>
2015-05-19 11:12:28 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -1,13 +1,14 @@
 .text
 .align 2
-.global main0
-.type main0, %function
-main0:
+.global main
+.type main, %function
+
+main:
 @Obteniendo los parametros y guardandolos en memoria
 LDR R0,=a
 LDR R0, [R0]
 MOV R1, #12
-STR R1, =a
+STR R1, [R0]
 ldmfd sp!, {lr}
 bx lr
 
