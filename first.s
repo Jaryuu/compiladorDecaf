bd01d9330cff07f2fa34fae8f2f3ea87e7cb56f6
Julio Ayala <ayala.j@live.com>
2015-05-25 07:35:45 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -6,24 +6,10 @@ base:
 .text
 .global main
 main:
-@Obteniendo los parametros y guardandolos en memoria
+/*Obteniendo los parametros y guardandolos en memoria*/
 LDR R0, addr_base
-ADD R0, R0, #0
 MOV R1, #10
-STR R1, [R0]
-LDR R0, addr_base
-ADD R0, R0, #4
-MOV R1, #12
-STR R1, [R0]
-LDR R0, addr_base
-ADD R0, R0, #4
-LDR R1, addr_base
-LDR R1, [R1,#4]
-LDR R2, addr_base
-LDR R2, [R2,#0]
-ADD R1, R1, R2
-STR R1, [R0]
-@HALT
-ldmfd sp!, {lr}
+STR R1, [R0,#0]
+/*Fin codigo*/
 bx lr
 addr_base : .word base
