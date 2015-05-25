e68934135e212cd19b40d3f857dda83f91cbaac4
Julio Ayala <ayala.j@live.com>
2015-05-25 07:38:49 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -10,6 +10,20 @@ main:
 LDR R0, addr_base
 MOV R1, #10
 STR R1, [R0,#0]
+LDR R0, addr_base
+MOV R1, #15
+STR R1, [R0,#4]
+LDR R0, addr_base
+LDR R1, addr_base
+LDR R1, [R1,#4]
+LDR R2, addr_base
+LDR R2, [R2,#0]
+ADD R1, R1, R2
+STR R1, [R0,#0]
+LDR R0, addr_base
+LDR R1, addr_base
+LDR R1, [R1,#4]
+STR R1, [R0,#4]
 /*Fin codigo*/
 bx lr
 addr_base : .word base
