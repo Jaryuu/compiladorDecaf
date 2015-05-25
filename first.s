55bee5d182bf358759d8e7bb2da7307a6f01a3e6
Julio Ayala <ayala.j@live.com>
2015-05-25 07:51:54 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -1,10 +1,7 @@
-.data
-.balign 4
-base:
-.word 0
-
 .text
 .global main
+.global printf
+
 main:
 /*Obteniendo los parametros y guardandolos en memoria*/
 LDR R0, addr_base
@@ -20,10 +17,12 @@ LDR R2, addr_base
 LDR R2, [R2,#0]
 ADD R1, R1, R2
 STR R1, [R0,#0]
-LDR R0, addr_base
-LDR R1, addr_base
-LDR R1, [R1,#4]
-STR R1, [R0,#4]
+BL printf;
+
+
 /*Fin codigo*/
 bx lr
 addr_base : .word base
+
+
+
