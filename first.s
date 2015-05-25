e7c229645fbaf09c247b7a82ace52e0a5c1317da
Julio Ayala <ayala.j@live.com>
2015-05-25 08:48:43 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -5,28 +5,30 @@ base:
 
 .text
 .global main
-
-
 main:
 /*Obteniendo los parametros y guardandolos en memoria*/
 LDR R0, addr_base
-MOV R1, #10
+MOV R1, #1
+MOV R2, #2
+CMP R1, R2
+MOVGT R1, #1
+MOVLEQ R1, #0
+MOV R2, #3
+MOV R3, #4
+CMP R2, R3
+MOVGT R2, #1
+MOVLEQ R2, #0
+MOV R3, #5
+MOV R4, #6
+ADD R3, R3, R4
+MOV R4, #7
+CMP R3, R4
+MOVGT R3, #1
+MOVLEQ R3, #0
+AND R2, R2, R3
+ORR R1, R1, R2
 STR R1, [R0,#0]
-LDR R0, addr_base
-MOV R1, #15
-STR R1, [R0,#4]
-LDR R0, addr_base
-LDR R1, addr_base
-LDR R1, [R1,#4]
-LDR R2, addr_base
-LDR R2, [R2,#0]
-ADD R1, R1, R2
-STR R1, [R0,#4]
-LDR R0, addr_base
-LDR R0, [R0,#4]
-
+MOV R0, R1
 /*Fin codigo*/
 bx lr
 addr_base : .word base
-
-.global puts
