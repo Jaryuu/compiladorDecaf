596465e5b1cfdf7e57bd9ef5e220bd6502186c78
Julio Ayala <ayala.j@live.com>
2015-05-26 11:01:43 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -22,12 +22,14 @@ LDR R0, [R0,#0]
 LDR R1, addr_base
 LDR R1, [R1,#1]
 ORR R0, R0, R1
-MOV R0, !R0
+CMP R0, #0
+MOVEQR0, #1
+MOVNER0, #0
 LDR R1, addr_base
 LDR R1, [R1,#2]
 ORR R0, R0, R1
 /*If que hace el salto*/
-CMP R0, 0
+CMP R0, #0
 BEQ label_0_false
 @ codigo del if
 LDR R1, addr_base
@@ -47,5 +49,6 @@ MOV R2, #200
 STR R2, [R1,#3]
 codeIf_0_next:
 /*Fin codigo main*/
+MOV R0, R2
 bx lr
 addr_base : .word base
