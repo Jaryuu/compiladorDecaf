6a161cd3c35fdbd3bf1babec6054fb183c38cbf3
Julio Ayala <ayala.j@live.com>
2015-05-26 17:46:18 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -5,35 +5,105 @@ base:
 
 .text
 .global main
-metodo0:
+fib1:
 /*Obteniendo los parametros y guardandolos en memoria*/
-MOV R0, R12
-MOV R1, #12
-MOV R2, #15
-ADD R1, R1, R2
-STR R1, [R0,#4]
-MOV R0, R12
-LDR R0, [R0,#4]
+POP {R0}
+MOV R1, R12
+STR R0, [R1,#0]
+@Empieza aqui el If
+LDR R0, R12
+LDR R0, [R0,#0]
+MOV R1, #0
+CMP R0, R1
+MOVEQ R0, #1
+MOVNE R0, #0
+LDR R1, R12
+LDR R1, [R1,#0]
+MOV R2, #1
+CMP R1, R2
+MOVEQ R1, #1
+MOVNE R1, #0
+ORR R0, R0, R1
+/*If que hace el salto*/
+CMP R0, #0
+BEQ label_0_false
+@ codigo del if
+LDR R1, R12
+MOV R2, #1
+STR R2, [R1,#4]
+B codeIf_0_next
+@ codigo del else
+label_0_false:
+LDR R1, R12
+@Agregar estado actual a la pila
+PUSH {R0}
+PUSH {R1}
+PUSH {LR}
+ADD R12, R12, #16
+LDR R2, R12
+LDR R2, [R2,#0]
+MOV R3, #1
+DIF R2, R2, R3
+PUSH {R2}
+BL fib1
+label0:
+POP {R3}
+POP {R1}
+POP {R0}
+SUB R12, R12, #16
+STR R3, [R2,#8]
+LDR R2, R12
+@Agregar estado actual a la pila
+PUSH {R0}
+PUSH {R1}
+PUSH {R2}
+PUSH {LR}
+ADD R12, R12, #16
+LDR R3, R12
+LDR R3, [R3,#0]
+MOV R4, #2
+DIF R3, R3, R4
+PUSH {R3}
+BL fib1
+label1:
+POP {R4}
+POP {R2}
+POP {R1}
+POP {R0}
+SUB R12, R12, #16
+STR R4, [R3,#12]
+LDR R3, R12
+LDR R4, R12
+LDR R4, [R4,#8]
+LDR R5, R12
+LDR R5, [R5,#12]
+ADD R4, R4, R5
+STR R4, [R3,#4]
+codeIf_0_next:
+LDR R3, R12
+LDR R3, [R3,#4]
 /*Retornando a donde se llamo al metodo.*/
 POP {LR}
-PUSH {R0}
+PUSH {R3}
 BX LR
 
 main:
 LDR R12, addr_base
 /*Obteniendo los parametros y guardandolos en memoria*/
-MOV R0, R12
+LDR R0, R12
 @Agregar estado actual a la pila
 PUSH {R0}
 PUSH {LR}
-ADD R12, R12, #12
-BL metodo0
-label0:
-POP {R1}
+ADD R12, R12, #4
+MOV R1, #4
+PUSH {R1}
+BL fib1
+label2:
+POP {R2}
 POP {R0}
-SUB R12, R12, #12
-STR R1, [R0,#8]
+SUB R12, R12, #4
+STR R2, [R1,#0]
 /*Fin codigo main*/
-MOV R0, R1
+MOV R0,R2
 BX LR
 addr_base : .word base
