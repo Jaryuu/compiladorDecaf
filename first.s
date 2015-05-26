2763c597be7af89cf2d4c268c499969cfea1d31a
Julio Ayala <ayala.j@live.com>
2015-05-26 17:01:46 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -5,41 +5,34 @@ base:
 
 .text
 .global main
-main:
+metodo0:
 /*Obteniendo los parametros y guardandolos en memoria*/
-LDR R0, addr_base
-MOV R1, #1
-STR R1, [R0,#0]
-LDR R0, addr_base
-MOV R1, #0
+LDR R0, R12
+MOV R1, #12
+MOV R2, #15
+ADD R1, R1, R2
 STR R1, [R0,#4]
-LDR R0, addr_base
-MOV R1, #1
-STR R1, [R0,#8]
-@Empieza aqui el If
-LDR R0, addr_base
+LDR R0, R12
 LDR R0, [R0,#4]
-/*If que hace el salto*/
-CMP R0, #0
-BEQ label_0_false
-@ codigo del if
-LDR R1, addr_base
-MOV R2, #12
-STR R2, [R1,#16]
-LDR R1, addr_base
-MOV R2, #100
-STR R2, [R1,#12]
-B codeIf_0_next
-@ codigo del else
-label_0_false:
-LDR R1, addr_base
-MOV R2, #19
-STR R2, [R1,#20]
-LDR R1, addr_base
-MOV R2, #200
-STR R2, [R1,#12]
-codeIf_0_next:
+/*Retornando a donde se llamo al metodo.*/
+POP {LR}
+PUSH {R0}
+BX LR
+
+main:
+LDR R12, addr_base
+/*Obteniendo los parametros y guardandolos en memoria*/
+LDR R0, R12
+@Agregar estado actual a la pila
+PUSH {R0}
+PUSH {LR}
+ADD R12, R12, #12
+BL metodo0
+label0:
+POP {R1}
+POP {R0}
+SUB R12, R12, #12
+STR R1, [R0,#8]
 /*Fin codigo main*/
-MOV R0, R2
-bx lr
+BX LR
 addr_base : .word base
