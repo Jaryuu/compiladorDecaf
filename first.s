aaff69aee30590c767028a84fc8640120a08a705
Julio Ayala <ayala.j@live.com>
2015-05-26 05:31:42 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -15,22 +15,22 @@ STR R1, [R0,#4]
 LDR R0, addr_base
 LDR R0, [R0,#4]
 /*Retornando a donde se llamo al metodo.*/
-POP LR
-PUSH R0
+POP {LR}
+PUSH {R0}
 BX LR
 
 main:
 /*Obteniendo los parametros y guardandolos en memoria*/
 LDR R0, addr_base
 @Agregar estado actual a la pila
-PUSH R0
-PUSH R1
+PUSH {R0}
+PUSH {R1}
 @PUSH label0 esto no
-PUSH LR
+PUSH {LR}
 BL metodo0
 label0:
-POP R1
-POP R0
+POP {R1}
+POP {R0}
 STR R1, [R0,#8]
 LDR R0, [R0,#8]
 /*Fin codigo*/
