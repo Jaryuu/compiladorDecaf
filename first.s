a03bf3cfb98d3c7bbf9bba90640fca3e9995e5ec
Julio Ayala <ayala.j@live.com>
2015-05-26 19:29:02 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -5,58 +5,28 @@ base:
 
 .text
 .global main
-metodo1:
-/*Obteniendo los parametros y guardandolos en memoria*/
-POP {R0}
-MOV R1, R12
-STR R0, [R1,#0]
-MOV R0, R12
-MOV R1, #90
-STR R1, [R0,#0]
-MOV R0, R12
-MOV R1, #11
-STR R1, [R0,#4]
-MOV R0, R12
-MOV R1, #12
-STR R1, [R0,#0]
-MOV R0, R12
-LDR R0, [R0,#0]
-/*Retornando a donde se llamo al metodo.*/
-POP {PC}
-PUSH {R0}
-BX LR
-
-metodo21:
-/*Obteniendo los parametros y guardandolos en memoria*/
-POP {R0}
-MOV R1, R12
-STR R0, [R1,#0]
-MOV R0, #56
-/*Retornando a donde se llamo al metodo.*/
-POP {PC}
-PUSH {R0}
-BX LR
-
 main:
-LDR R12, addr_base
+MOV R12, addr_base
 /*Obteniendo los parametros y guardandolos en memoria*/
 MOV R0, R12
-MOV R1, #1
-STR R1, [R0,#0]
+MOV R1, #10
+STR R1, [R0,#40]
+MOV R0, #3
+MOV R1, #0
+MOV R3, #4
+MUL R2, R0,  R3
+ADD R0, R1, R2
+MOV R1, R12
+LDR R1, [R1,#40]
+STR R1, [R0,#40]
 MOV R0, R12
-@Agregar estado actual a la pila
-PUSH {R0}
-PUSH {LR}
-MOV R1, #12
-PUSH {R1}
-ADD R12, R12, #12
-BL metodo1
-label0:
-POP {R2}
-POP {R0}
-SUB R12, R12, #12
-STR R2, [R1,#8]
+MOV R1, #3
+MOV R2, #0
+MOV R4, #4
+MUL R3, R1,  R4
+ADD R1, R2, R3
+STR R1, [R0,#44]
 /*Fin codigo main*/
-MOV R0, R2
+MOV R0, R1
 BX LR
 addr_base : .word base
