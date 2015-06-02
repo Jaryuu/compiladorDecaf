08ba2c648ccf739277b596b83cae9e1109e3d322
Julio Ayala <ayala.j@live.com>
2015-06-02 10:49:10 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -2,7 +2,7 @@
 .global main
 main:
 stmfd sp!, {lr}
-LDR R11, =base
+LDR R11, addr_base
 push {lr}
 BL main0
 B exit
@@ -67,10 +67,12 @@ LDR R5, [R5,#0]
 MOV R6, #1
 SUB R5, R5, R6
 PUSH {R5}
+ADD R11, R11, #16
 BL fibonacci1
 POP {R6}
 POP {R4}
 POP {R3}
+SUB R11, R11, #16
 STR R6, [R5,#4]
 MOV R5, R11
 @Agregar estado actual a la pila
@@ -82,11 +84,13 @@ LDR R6, [R6,#0]
 MOV R7, #2
 SUB R6, R6, R7
 PUSH {R6}
+ADD R11, R11, #16
 BL fibonacci1
 POP {R7}
 POP {R5}
 POP {R4}
 POP {R3}
+SUB R11, R11, #16
 STR R7, [R6,#8]
 MOV R6, R11
 MOV R7, R11
@@ -131,10 +135,12 @@ PUSH {R4}
 MOV R5, R11
 LDR R5, [R5,#4]
 PUSH {R5}
+ADD R11, R11, #8
 BL fibonacci1
 POP {R6}
 POP {R4}
 POP {R3}
+SUB R11, R11, #8
 STR R6, [R5,#0]
 MOV R5, R11
 MOV R6, R11
@@ -155,4 +161,5 @@ BX LR
 .align 4
 int: .asciz "%d\n"
 char: .asciz "%s\n"
-base:	.space 1024
+base: .word 0
+addr_base: .word base
