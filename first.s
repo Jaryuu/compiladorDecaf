0da3856b93d6e21526e9dff80c8b5cab2f305586
Julio Ayala <ayala.j@live.com>
2015-06-02 15:54:40 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -41,25 +41,46 @@ fibonacci1:
 POP {R3}
 STR R3, [R11, #0]
 PUSH {LR}
-LDR R3, [R11, #0]
-LDR R0, =int
-MOV R1, R3
-BL printf
 @Empieza aqui el If
-LDR R4, [R11, #0]
-MOV R5, #2
-CMP R4, R5
-MOVLT R4, #1
-MOVGE R4, #0
+LDR R3, [R11, #0]
+MOV R4, #2
+CMP R3, R4
+MOVLT R3, #1
+MOVGE R3, #0
 /*If que hace el salto*/
-CMP R4, #0
+CMP R3, #0
 BEQ label_0_false
 @ codigo del if
-MOV R6, #1
-STR R6, [R11, #12]
+MOV R5, #1
+STR R5, [R11, #12]
 B codeIf_0_next
 @ codigo del else
 label_0_false:
+@Agregar estado actual a la pila
+/*Guardando todas las var. locales*/
+LDR R5, [R11, #0]
+PUSH {R5}
+LDR R5, [R11, #4]
+PUSH {R5}
+LDR R5, [R11, #8]
+PUSH {R5}
+LDR R5, [R11, #12]
+PUSH {R5}
+LDR R5, [R11, #0]
+MOV R6, #1
+SUB R5, R5, R6
+PUSH {R5}
+BL fibonacci1
+POP {R6}
+POP {R7}
+STR R7, [R11, #12]
+POP {R7}
+STR R7, [R11, #8]
+POP {R7}
+STR R7, [R11, #4]
+POP {R7}
+STR R7, [R11, #0]
+STR R6, [R11, #4]
 @Agregar estado actual a la pila
 /*Guardando todas las var. locales*/
 LDR R6, [R11, #0]
@@ -71,7 +92,7 @@ PUSH {R6}
 LDR R6, [R11, #12]
 PUSH {R6}
 LDR R6, [R11, #0]
-MOV R7, #1
+MOV R7, #2
 SUB R6, R6, R7
 PUSH {R6}
 BL fibonacci1
@@ -84,42 +105,17 @@ POP {R8}
 STR R8, [R11, #4]
 POP {R8}
 STR R8, [R11, #0]
-STR R7, [R11, #4]
-@Agregar estado actual a la pila
-/*Guardando todas las var. locales*/
-LDR R7, [R11, #0]
-PUSH {R7}
+STR R7, [R11, #8]
 LDR R7, [R11, #4]
-PUSH {R7}
-LDR R7, [R11, #8]
-PUSH {R7}
-LDR R7, [R11, #12]
-PUSH {R7}
-LDR R7, [R11, #0]
-MOV R8, #2
-SUB R7, R7, R8
-PUSH {R7}
-BL fibonacci1
-POP {R8}
-POP {R9}
-STR R9, [R11, #12]
-POP {R9}
-STR R9, [R11, #8]
-POP {R9}
-STR R9, [R11, #4]
-POP {R9}
-STR R9, [R11, #0]
-STR R8, [R11, #8]
-LDR R8, [R11, #4]
-LDR R9, [R11, #8]
-ADD R8, R8, R9
-STR R8, [R11, #12]
+LDR R8, [R11, #8]
+ADD R7, R7, R8
+STR R7, [R11, #12]
 codeIf_0_next:
-LDR R7, [R11, #12]
+LDR R6, [R11, #12]
 /*Retornando a donde se llamo al metodo.*/
-POP {R8}
-PUSH {R7}
-MOV PC, R8
+POP {R7}
+PUSH {R6}
+MOV PC, R7
 main0:
 /*Obteniendo los parametros y guardandolos en memoria*/
 PUSH {LR}
@@ -130,8 +126,8 @@ begin_0:
 LDR R3, [R11, #4]
 MOV R4, #10
 CMP R3, R4
-MOVLE R3, #1
-MOVGT R3, #0
+MOVLT R3, #1
+MOVGE R3, #0
 /*If que hace el salto*/
 CMP R3, #0
 BEQ codeWhile_0_next
