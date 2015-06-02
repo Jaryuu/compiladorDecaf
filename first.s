c75ebfe8eb5e877077e0fe8718193a58b892914a
Julio Ayala <ayala.j@live.com>
2015-06-02 11:05:25 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -42,8 +42,7 @@ POP {R3}
 STR R3, [R11, #0]
 PUSH {LR}
 @Empieza aqui el If
-MOV R3, R11
-LDR R3, [R3,#0]
+LDR R3, [R11, #0]
 MOV R4, #2
 CMP R3, R4
 MOVLT R3, #1
@@ -52,18 +51,15 @@ MOVGE R3, #0
 CMP R3, #0
 BEQ label_0_false
 @ codigo del if
-MOV R4, R11
 MOV R5, #1
 STR R5, [R4,#12]
 B codeIf_0_next
 @ codigo del else
 label_0_false:
-MOV R4, R11
 @Agregar estado actual a la pila
 PUSH {R3}
 PUSH {R4}
-MOV R5, R11
-LDR R5, [R5,#0]
+LDR R5, [R11, #0]
 MOV R6, #1
 SUB R5, R5, R6
 PUSH {R5}
@@ -72,13 +68,11 @@ POP {R6}
 POP {R4}
 POP {R3}
 STR R6, [R5,#4]
-MOV R5, R11
 @Agregar estado actual a la pila
 PUSH {R3}
 PUSH {R4}
 PUSH {R5}
-MOV R6, R11
-LDR R6, [R6,#0]
+LDR R6, [R11, #0]
 MOV R7, #2
 SUB R6, R6, R7
 PUSH {R6}
@@ -88,21 +82,16 @@ POP {R5}
 POP {R4}
 POP {R3}
 STR R7, [R6,#8]
-MOV R6, R11
-MOV R7, R11
-LDR R7, [R7,#4]
-MOV R8, R11
-LDR R8, [R8,#8]
+LDR R7, [R11, #4]
+LDR R8, [R11, #8]
 ADD R7, R7, R8
 STR R7, [R6,#12]
-MOV R6, R11
-LDR R6, [R6,#12]
+LDR R6, [R11, #12]
 LDR R0, =int
 MOV R1, R6
 BL printf
 codeIf_0_next:
-MOV R7, R11
-LDR R7, [R7,#12]
+LDR R7, [R11, #12]
 /*Retornando a donde se llamo al metodo.*/
 POP {R8}
 PUSH {R7}
@@ -110,13 +99,11 @@ MOV PC, R8
 main0:
 /*Obteniendo los parametros y guardandolos en memoria*/
 PUSH {LR}
-MOV R3, R11
 MOV R4, #0
 STR R4, [R3,#4]
 @Empieza aqui el while
 begin_0:
-MOV R3, R11
-LDR R3, [R3,#4]
+LDR R3, [R11, #4]
 MOV R4, #40
 CMP R3, R4
 MOVLE R3, #1
@@ -124,21 +111,17 @@ MOVGT R3, #0
 /*If que hace el salto*/
 CMP R3, #0
 BEQ codeWhile_0_next
-MOV R4, R11
 @Agregar estado actual a la pila
 PUSH {R3}
 PUSH {R4}
-MOV R5, R11
-LDR R5, [R5,#4]
+LDR R5, [R11, #4]
 PUSH {R5}
 BL fibonacci1
 POP {R6}
 POP {R4}
 POP {R3}
 STR R6, [R5,#0]
-MOV R5, R11
-MOV R6, R11
-LDR R6, [R6,#4]
+LDR R6, [R11, #4]
 MOV R7, #1
 ADD R6, R6, R7
 STR R6, [R5,#4]
