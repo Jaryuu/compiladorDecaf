a19bba4da18afe2e11fd56e394e1e1bb39cbb78b
Julio Ayala <ayala.j@live.com>
2015-06-02 07:29:58 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -10,15 +10,28 @@ main:
 LDR R12, addr_base
 /*Obteniendo los parametros y guardandolos en memoria*/
 MOV R3, R12
-MOV R4, #2
-MOV R5, #4
-MOV R6, #3
-MUL R5, R5, R6
-ADD R4, R4, R5
+MOV R4, #0
 STR R4, [R3,#0]
+@Empieza aqui el while
+begin_0:
 MOV R3, R12
-LDR R0, [R3,#0]
-BL printf
+LDR R3, [R3,#0]
+MOV R4, #10
+CMP R3, R4
+MOVLT R3, #1
+MOVGE R3, #0
+/*If que hace el salto*/
+CMP R3, #0
+BEQ codeWhile_0_next
+MOV R4, R12
+MOV R5, R12
+LDR R5, [R5,#0]
+MOV R6, #1
+ADD R5, R5, R6
+STR R5, [R4,#0]
+B begin_0
+codeWhile_0_next:
+MOV R0, R5;
 /*Fin codigo main*/
 BX LR
 addr_base : .word base
