6305d7f7160f615c0e9fe67430e1fa19fa314286
Julio Ayala <ayala.j@live.com>
2015-06-02 07:54:55 -0600

    nuevo archivo

--- first.s
+++ first.s
@@ -2,14 +2,14 @@
 .global main
 main:
 stmfd sp!, {lr}
-LDR R12, =base
+LDR R11, =base
 /*Obteniendo los parametros y guardandolos en memoria*/
-MOV R3, R12
+MOV R3, R11
 MOV R4, #0
 STR R4, [R3,#0]
 @Empieza aqui el while
 begin_0:
-MOV R3, R12
+MOV R3, R11
 LDR R3, [R3,#0]
 MOV R4, #10
 CMP R3, R4
@@ -18,8 +18,8 @@ MOVGE R3, #0
 /*If que hace el salto*/
 CMP R3, #0
 BEQ codeWhile_0_next
-MOV R4, R12
-MOV R5, R12
+MOV R4, R11
+MOV R5, R11
 LDR R5, [R5,#0]
 MOV R6, #1
 ADD R5, R5, R6
